ba67229529d00b80497a676bad5c0e04f989d629
Fina <finapani@gmail.com>
2016-02-14 22:52:53 -0500

    Bit more reorganization. Notes.

--- app/header.php
+++ app/header.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"> -->
 	<script type="text/javascript" src="app/js/main.min.js"></script>
 </head>
 <body>
