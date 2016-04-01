be6aa7e6c0b9da2bfc1553beaca352993387065b
Fina <finapani@gmail.com>
2016-04-01 19:30:20 -0400

    Forgot to disable some empty test classes.

--- test/unit/api/database/DatabaseSpec.php
+++ test/unit/api/database/DatabaseSpec.php
@@ -3,35 +3,35 @@ namespace SP\Test\Unit\Database;
 
 use SP\App\Api\Database;
 
-class DatabaseTest extends \PHPUnit_Extensions_Database_TestCase
-{
-    private $conn = null;
+// class DatabaseTest extends \PHPUnit_Extensions_Database_TestCase
+// {
+//     private $conn = null;
 
-    public function getConnection()
-    {
-        $this->conn = new PDO(
-            'mysql:host=127.0.0.1;dbname=test;charset=utf8mb4',
-            'root',
-            ''
-        );
-        return $this->createDefaultDBConnection($this->conn, 'test');
-    }
+//     public function getConnection()
+//     {
+//         $this->conn = new PDO(
+//             'mysql:host=127.0.0.1;dbname=test;charset=utf8mb4',
+//             'root',
+//             ''
+//         );
+//         return $this->createDefaultDBConnection($this->conn, 'test');
+//     }
 
-    public function getDataSet()
-    {
-        return $this->createMySQLXMLDataSet('dbTest.xml');
-    }
+//     public function getDataSet()
+//     {
+//         return $this->createMySQLXMLDataSet('dbTest.xml');
+//     }
 
-    public function testDatabaseStub()
-    {
-        $stub = $this->getMockBuilder('Database')
-            ->setMethods(array('query'))
-            ->getMock();
+//     public function testDatabaseStub()
+//     {
+//         $stub = $this->getMockBuilder('Database')
+//             ->setMethods(array('query'))
+//             ->getMock();
 
-        // TODO: redefine query to use php db test, pass into crud types
-        // $stub->expects($this->once())
-        //     ->method('query')
-        //     ->withAnyParameters()
-        //     ->will($this->returnCallback(/*function name*/));
-    }
-}
+//         // TODO: redefine query to use php db test, pass into crud types
+//         // $stub->expects($this->once())
+//         //     ->method('query')
+//         //     ->withAnyParameters()
+//         //     ->will($this->returnCallback(/*function name*/));
+//     }
+// }
